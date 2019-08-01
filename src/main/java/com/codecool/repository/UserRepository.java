91c3612c3dc54845582deb7b3167562263d8c57c
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-01 17:03:43 +0200

    annoteded interface as repository

--- src/main/java/com/codecool/repository/UserRepository.java
+++ src/main/java/com/codecool/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.codecool.repository;
 
 import com.codecool.model.Users;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<Users, Integer> {
 
     Optional<Users> findByName(String name);

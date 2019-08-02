84929b7a0c78084cae12a12b57489a50b1566ca1
Gergő Papp <imgergopapp@gmail.com>
2019-08-02 11:22:34 +0200

    Add CrossOrigin annotation (CROS)

--- src/main/java/com/codecool/controller/AuthController.java
+++ src/main/java/com/codecool/controller/AuthController.java
@@ -12,10 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -39,7 +36,8 @@ public class AuthController {
         this.jwtTokenServices = jwtTokenServices;
     }
 
-    @PostMapping("/login")
+    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+    @PostMapping("/signin")
     public ResponseEntity login(@RequestBody UserCredentials data) {
         try {
             String name = data.getName();
@@ -61,3 +59,5 @@ public class AuthController {
         }
     }
 }
+
+

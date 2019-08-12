6f7769bd837651970809ba99daccf1ffe1ce2783
zeeKovacs <zolee95@gmail.com>
2019-08-12 10:58:46 +0200

    fix internal error during reg

--- src/main/java/com/codecool/controller/RegisterController.java
+++ src/main/java/com/codecool/controller/RegisterController.java
@@ -3,12 +3,12 @@ package com.codecool.controller;
 import com.codecool.model.Users;
 import com.codecool.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RegisterController {
     @Autowired
     private UserRepository userRepository;
@@ -21,7 +21,6 @@ public class RegisterController {
         String email = data.getEmail();
         String location = data.getLocation();
         String address = data.getAddress();
-        String[] roles = new String[]{"USER"};
         Users newUser = new Users(name, email, password, location, address);
         return userRepository.save(newUser);
     }

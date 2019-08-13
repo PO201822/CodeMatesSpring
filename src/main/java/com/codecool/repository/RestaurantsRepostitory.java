a86a7b4d35b1e3dfcfb50b1128bf194b4c364d1b
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-13 10:20:46 +0200

    Created repository class

---
+++ src/main/java/com/codecool/repository/RestaurantsRepostitory.java
@@ -0,0 +1,13 @@
+package com.codecool.repository;
+
+import com.codecool.model.Restaurants;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RestaurantsRepostitory extends JpaRepository<Restaurants, Integer> {
+
+    List<Restaurants> findByLocationIs(String location);
+}

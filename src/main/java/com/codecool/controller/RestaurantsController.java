16e209dccec8abbb4818ba36f84eac33d67a0576
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-14 14:35:10 +0200

    added restaurants endpoint

--- src/main/java/com/codecool/controller/RestaurantsController.java
+++ src/main/java/com/codecool/controller/RestaurantsController.java
@@ -1,9 +1,9 @@
 package com.codecool.controller;
 
 import com.codecool.model.Restaurants;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.repository.RestaurantsRepostitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -11,13 +11,15 @@ import javax.persistence.PersistenceContext;
 @RestController
 public class RestaurantsController {
     @PersistenceContext
-    public EntityManager em;
+    private EntityManager em;
+
+    @Autowired
+    private RestaurantsRepostitory restaurantsRepostitory;
 
     @GetMapping(path="/restaurants")
     public @ResponseBody
-    Iterable<Restaurants> getRestaurantWhereProductNameIs() {
-        Iterable<Restaurants> r = em.createNamedQuery("findRatedRestaurants").setParameter("iduser", 2).getResultList();
-        return r;
+    Iterable<Restaurants> getRestaurantsWhereLocationIs(@RequestParam(value = "location") String location) {
+        String trimmedLoc = location.replaceAll("\"", "");
+        return restaurantsRepostitory.findByLocationIgnoreCase(trimmedLoc);
     }
-
 }

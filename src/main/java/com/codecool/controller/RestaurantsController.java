f06f6b33d9aa7784f1890def42f08f0e05fece3d
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-15 11:33:23 +0200

    Added getmapping to /restaurant/id endpoint

--- src/main/java/com/codecool/controller/RestaurantsController.java
+++ src/main/java/com/codecool/controller/RestaurantsController.java
@@ -1,5 +1,7 @@
 package com.codecool.controller;
 
+import com.codecool.dto.ProductDto;
+import com.codecool.model.Products;
 import com.codecool.model.Restaurants;
 import com.codecool.repository.RestaurantsRepostitory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @RestController
 public class RestaurantsController {
@@ -21,5 +25,14 @@ public class RestaurantsController {
     Iterable<Restaurants> getRestaurantsWhereLocationIs(@RequestParam(value = "location") String location) {
         String trimmedLoc = location.replaceAll("\"", "");
         return restaurantsRepostitory.findByLocationIgnoreCase(trimmedLoc);
+    }
+
+    @GetMapping(path="/restaurant/{id}")
+    public @ResponseBody
+    List<Products> getRestaurantsWhereLocationIs(@PathVariable(value = "id") int id) {
+        TypedQuery<Products> query =
+                em.createNamedQuery("findAllByRestaurantId", Products.class).setParameter("restaurantid", id);
+        List<Products> p = query.getResultList();
+        return p;
     }
 }

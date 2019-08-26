6d07e46d5784e0f10ff2806aa73006dd9c364b2f
Gergő Papp <imgergopapp@gmail.com>
2019-08-26 09:34:21 +0200

    Implement "email already exists" check

--- src/main/java/com/codecool/controller/RegisterController.java
+++ src/main/java/com/codecool/controller/RegisterController.java
@@ -3,9 +3,12 @@ package com.codecool.controller;
 import com.codecool.model.Users;
 import com.codecool.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class RegisterController {
@@ -19,7 +22,13 @@ public class RegisterController {
         String email = data.getEmail();
         String location = data.getLocation();
         String address = data.getAddress();
-        Users newUser = new Users(name, email, password, location, address);
-        return userRepository.save(newUser);
+
+        if(userRepository.findByEmail(email).equals(Optional.empty()) ){
+            Users newUser = new Users(name, email, password, location, address);
+            return userRepository.save(newUser);
+        }
+        else {
+            throw new BadCredentialsException("Email already exists!");
+        }
     }
 }

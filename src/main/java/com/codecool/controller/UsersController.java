0288e4a82a6d7a966094af815c8ae26cb3b04ae1
Gergő Papp <imgergopapp@gmail.com>
2019-08-27 11:29:53 +0200

    Fix user's role on profile update

--- src/main/java/com/codecool/controller/UsersController.java
+++ src/main/java/com/codecool/controller/UsersController.java
@@ -3,12 +3,13 @@ package com.codecool.controller;
 import com.codecool.model.Users;
 import com.codecool.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.Optional;
 
 @RestController
 public class UsersController {
@@ -22,19 +23,15 @@ public class UsersController {
     @PutMapping(path = "/profile")
     public @ResponseBody
     void updateProfile(@RequestBody Users user) {
-        int a = 1;
-
         Users userBU = userRepository.findByEmail(user.getEmail());
 
         userBU.setName(user.getName());
         userBU.setAddress(user.getAddress());
         userBU.setPassword(user.getPassword());
         userBU.setLocation(user.getLocation());
-
+        userBU.setRoles(user.getRoles());
 
         userRepository.save(userBU);
-
-
 
     }
 }

91a135cb0e33e8a61b38c1b1d0ccdcb1ab2ee751
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-29 12:30:32 +0200

    implemented basic dto

---
+++ src/main/java/com/codecool/model/CartDto.java
@@ -0,0 +1,13 @@
+package com.codecool.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDto {
+    private Products products;
+    private int quantity;
+}

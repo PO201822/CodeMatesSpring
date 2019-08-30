ef50a8c219000167a2c56ca7aa6954528228cf6a
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-30 09:52:17 +0200

    chenged profile updating logic, you can update your email instead of username, because token auth on loading profile works with name, and if you would change your name the name in the token wouldnt change

--- src/main/java/com/codecool/controller/UsersController.java
+++ src/main/java/com/codecool/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.codecool.controller;
 import com.codecool.model.Users;
 import com.codecool.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,12 +24,13 @@ public class UsersController {
     @PutMapping(path = "/profile")
     public @ResponseBody
     void updateProfile(@RequestBody Users user) {
-        Users userBU = userRepository.findByEmail(user.getEmail());
+        Users userBU = userRepository.findByName(user.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("Username: " + user.getName() + " not found"));;
 
-        userBU.setName(user.getName());
         userBU.setAddress(user.getAddress());
         userBU.setPassword(user.getPassword());
         userBU.setLocation(user.getLocation());
+        userBU.setEmail(user.getEmail());
         userBU.setRoles(user.getRoles());
 
         userRepository.save(userBU);

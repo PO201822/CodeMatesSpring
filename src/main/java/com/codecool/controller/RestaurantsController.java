291a44739cb7fc464f050a824cac2042959ead1f
Gergő Papp <imgergopapp@gmail.com>
2019-09-06 21:40:06 +0200

    Change getRestaurantsWhereLocationIs method's return type to RestaurantsDto

--- src/main/java/com/codecool/controller/RestaurantsController.java
+++ src/main/java/com/codecool/controller/RestaurantsController.java
@@ -2,6 +2,7 @@ package com.codecool.controller;
 
 import com.codecool.model.Products;
 import com.codecool.model.Restaurants;
+import com.codecool.model.RestaurantsDto;
 import com.codecool.repository.RestaurantsRepostitory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,19 +20,20 @@ public class RestaurantsController {
     @Autowired
     private RestaurantsRepostitory restaurantsRepostitory;
 
-    @GetMapping(path="/restaurants")
+    @GetMapping(path = "/restaurants")
     public @ResponseBody
     Iterable<Restaurants> getRestaurantsWhereLocationIs(@RequestParam(value = "location") String location) {
         String trimmedLoc = location.replaceAll("\"", "");
         return restaurantsRepostitory.findByLocationIgnoreCase(trimmedLoc);
     }
 
-    @GetMapping(path="/restaurant/{id}")
+    @GetMapping(path = "/restaurant/{id}")
     public @ResponseBody
-    List<Products> getRestaurantsWhereLocationIs(@PathVariable(value = "id") int id) {
+    RestaurantsDto getRestaurantsWhereLocationIs(@PathVariable(value = "id") int id) {
         TypedQuery<Products> query =
                 em.createNamedQuery("findAllByRestaurantId", Products.class).setParameter("restaurantid", id);
-        List<Products> p = query.getResultList();
-        return p;
+        List<Products> products = query.getResultList();
+        Restaurants restaurant = restaurantsRepostitory.findById(id);
+        return new RestaurantsDto(products, restaurant);
     }
 }

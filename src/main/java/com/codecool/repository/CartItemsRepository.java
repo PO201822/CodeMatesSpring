17a31bcf3c75a1cbf9cef79dea748e36327e6a9d
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-09 18:44:53 +0200

    Implemented interface method to delete a product from users cart

--- src/main/java/com/codecool/repository/CartItemsRepository.java
+++ src/main/java/com/codecool/repository/CartItemsRepository.java
@@ -1,6 +1,8 @@
 package com.codecool.repository;
 
 import com.codecool.model.CartItems;
+import com.codecool.model.Products;
+import com.codecool.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface CartItemsRepository extends JpaRepository<CartItems, Integer> {
     @Override
     <S extends CartItems> S save(S entity);
+
+    void deleteByProductAndCart_User(Products p, Users user);
 }

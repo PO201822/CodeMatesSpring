186a09f62ccba83062a53b7e218e8aad440312a8
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-10 10:44:49 +0200

    findByName returning Users type

--- src/main/java/com/codecool/repository/UserRepository.java
+++ src/main/java/com/codecool/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<Users, Integer> {
 
-    Optional<Users> findByName(String name);
+    Users findByName(String name);
 
     Users findByEmail(String email);
 

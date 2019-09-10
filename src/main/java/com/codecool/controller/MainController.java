35149036b5aca50dfdb6cf7fe41f2882b0beae53
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-10 10:42:04 +0200

    Refactored

--- src/main/java/com/codecool/controller/MainController.java
+++ src/main/java/com/codecool/controller/MainController.java
@@ -1,38 +1,25 @@
 package com.codecool.controller;
 
-import com.codecool.model.JWTToken;
+import com.codecool.model.Users;
 import com.codecool.repository.UserRepository;
-import com.codecool.security.JwtTokenServices;
+import com.codecool.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 public class MainController {
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private UserRepository userRepository;
 
-    private JwtTokenServices jwtTokenServices;
-
-    MainController(JwtTokenServices jwtTokenServices) {
-        this.jwtTokenServices = jwtTokenServices;
-    }
-
     @PostMapping(path="/profile")
-    public Optional getProfile(@RequestBody JWTToken data) {
-        String token = data.getToken();
-        token = token.replaceAll("\"", "");
-        if (jwtTokenServices.validateToken(token)) {
-            Authentication auth = jwtTokenServices.parseUserFromTokenInfo(token);
-            String name = auth.getName();
-            return userRepository.findByName(name);
-        }
-        return null;
+    public Users getProfile() {
+        return userRepository.findByName(userService.currentUser());
     }
 }
 

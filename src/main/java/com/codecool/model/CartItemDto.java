0e64ea622e9e4bfc2540a61fac031da8adb47e7d
Gergő Papp <imgergopapp@gmail.com>
2019-09-10 22:10:34 +0200

    Implement CartItemDto class

---
+++ src/main/java/com/codecool/model/CartItemDto.java
@@ -0,0 +1,13 @@
+package com.codecool.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItemDto {
+    private int cartItemId;
+    private int quantity;
+}

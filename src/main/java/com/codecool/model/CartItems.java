913dfe75dd3818e851a7a5ce89ec9cfef25d390c
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-10 13:37:51 +0200

    created namedquerry, deleted senseless field

--- src/main/java/com/codecool/model/CartItems.java
+++ src/main/java/com/codecool/model/CartItems.java
@@ -16,6 +16,8 @@ import java.util.List;
 @NamedQueries({
         @NamedQuery(name = "findCartItemsByCartId",
                 query="select distinct ci from CartItems ci inner join ci.cart c where c.id = :cart_id"),
+        @NamedQuery(name = "deleteProductFromCart",
+                query="delete from CartItems ci where ci.product = :product and ci.cart = :cart"),
 })
 public class CartItems {
     @Id
@@ -34,10 +36,6 @@ public class CartItems {
     private Integer quantity;
 
     private Integer price;
-
-    @OneToMany(targetEntity = Orders.class)
-    private List<Orders> orders = new ArrayList<>();
-
 
     public CartItems(Carts cart, Products product, Integer quantity, Integer price) {
         this.cart = cart;

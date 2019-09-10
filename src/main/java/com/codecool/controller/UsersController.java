134b87dbdaec1f841d37e1d0cbf7cdb58bb519ca
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-10 10:43:17 +0200

    Refactored

--- src/main/java/com/codecool/controller/UsersController.java
+++ src/main/java/com/codecool/controller/UsersController.java
@@ -4,20 +4,14 @@ import com.codecool.model.Users;
 import com.codecool.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @RestController
 public class UsersController {
 
-    @PersistenceContext
-    private EntityManager em;
 
     @Autowired
     private UserRepository userRepository;
@@ -25,8 +19,7 @@ public class UsersController {
     @PutMapping(path = "/profile")
     public @ResponseBody
     void updateProfile(@RequestBody Users user) {
-        Users userBU = userRepository.findByName(user.getName())
-                .orElseThrow(() -> new UsernameNotFoundException("Username: " + user.getName() + " not found"));
+        Users userBU = userRepository.findByName(user.getName());
 
         if (!user.getEmail().equals(userBU.getEmail()) &&
                 userRepository.findByEmail(user.getEmail()) != null) {

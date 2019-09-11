740110cbebddea6c2ea9c6915ef5e50111d15748
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-11 13:44:35 +0200

    Created Dto

---
+++ src/main/java/com/codecool/dto/RoleDto.java
@@ -0,0 +1,13 @@
+package com.codecool.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDto {
+
+    private String role;
+}

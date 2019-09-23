c374fedff96f92d352b3e1dfb4c13de1c9bf789e
Gergő Papp <imgergopapp@gmail.com>
2019-09-23 16:29:17 +0200

    Extend CourierOrderDto with new constructor & completionDate field

--- src/main/java/com/codecool/dto/CourierOrderDto.java
+++ src/main/java/com/codecool/dto/CourierOrderDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,5 +13,11 @@ public class CourierOrderDto {
     private JobsDto jobsDto;
     private int orderId;
     private boolean complete;
+    private LocalDateTime completionDate;
 
+    public CourierOrderDto(JobsDto jobsDto, int orderId, boolean complete) {
+        this.jobsDto = jobsDto;
+        this.orderId = orderId;
+        this.complete = complete;
+    }
 }

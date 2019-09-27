c2819893a7f8e004d60a219b6bdd00c9b836dc9e
Gergő Papp <imgergopapp@gmail.com>
2019-09-27 08:56:20 +0200

    Extend CartOrderDto with new fields

--- src/main/java/com/codecool/dto/CartOrderDto.java
+++ src/main/java/com/codecool/dto/CartOrderDto.java
@@ -1,11 +1,14 @@
 package com.codecool.dto;
 
+import com.codecool.entity.CartItems;
 import com.codecool.entity.Carts;
+import com.codecool.entity.Users;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -14,5 +17,10 @@ public class CartOrderDto {
     private Carts cart;
     private boolean isComplete;
     private LocalDateTime completionDate;
+    private List<CartItems> cartItems;
+    private int quantity;
+    private int total;
+    private String status;
+    private Users costumer;
 
 }

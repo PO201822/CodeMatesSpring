bf21a1710d1d6632cb8ba26be827d2d630d032e4
Gergő Papp <imgergopapp@gmail.com>
2019-10-01 09:43:24 +0200

    Refactored CartOrderDto with CartItemDetail model

--- src/main/java/com/codecool/dto/CartOrderDto.java
+++ src/main/java/com/codecool/dto/CartOrderDto.java
@@ -3,24 +3,21 @@ package com.codecool.dto;
 import com.codecool.entity.CartItems;
 import com.codecool.entity.Carts;
 import com.codecool.entity.Users;
+import com.codecool.model.CartItemDetail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartOrderDto {
-    private Carts cart;
-    private boolean isComplete;
-    private LocalDateTime completionDate;
-    private List<CartItems> cartItems;
-    private int quantity;
-    private int total;
-    private String status;
-    private Users costumer;
 
+    private Carts cart;
+    private List<CartItems> cartItems;
+    private CartItemDetail cartItemDetail;
+    private Users costumer;
+    private String status;
 }

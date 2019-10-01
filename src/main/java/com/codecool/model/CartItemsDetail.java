4ea6543678c809f1a36880a4b93e5615f8050d94
Gergő Papp <imgergopapp@gmail.com>
2019-10-01 09:10:46 +0200

    Implement CartItemsDetail class

---
+++ src/main/java/com/codecool/model/CartItemsDetail.java
@@ -0,0 +1,16 @@
+package com.codecool.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class CartItemsDetail {
+
+    private int totalPrice;
+    private int quantity;
+
+}
